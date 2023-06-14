dee3ed9cea836a5b41df95dd9d7d97acac423833
Hugh O'Brien <github@hughobrien.ie>
2023-06-14 14:34:48 -0400

    inc

--- pkgs/development/libraries/openglidekjl/default.nix
+++ pkgs/development/libraries/openglidekjl/default.nix
@@ -1,5 +1,4 @@
-{ autoconf, automake, fetchFromGitHub, lib, libGL, libGLU, libtool, libX11, mesa
-, stdenv }:
+{ autoconf, automake, fetchFromGitHub, lib, libGL, libGLU, libtool, libX11, mesa , stdenv }:
 
 stdenv.mkDerivation rec {
   pname = "openglidekjl";
@@ -12,17 +11,15 @@ stdenv.mkDerivation rec {
     hash = "sha256-ePnFo7UhYzmir30w2B2rIvF9vlEBCOg+y/uGRO3MLJo=";
   };
 
-  nativeBuildInputs = [ automake autoconf libtool libGL ];
+  nativeBuildInputs = [ automake autoconf libtool ];
   buildInputs = [ libGL libGLU libX11 mesa ];
 
   sourceRoot = "source/openglide";
   preConfigure = "bash bootstrap";
-
   configureFlags = [ "--disable-sdl" ];
 
   meta = with lib; {
-    description =
-      "OpenGLide fork optimized for QEMU (Supports Glide2x & Glide3x)";
+    description = "OpenGLide fork optimized for QEMU (Supports Glide2x & Glide3x)";
     homepage = "https://github.com/kjliew/qemu-xtra";
     license = licenses.lgpl21;
     maintainers = with maintainers; [ hughobrien ];
