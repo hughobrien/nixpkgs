eed360e2d9ba493af3109b3fee4f2cf27df2d605
Hugh O'Brien <github@hughobrien.ie>
2023-06-17 04:47:30 -0400

    patcher9x: init at 0.8.50

---
+++ pkgs/development/tools/patcher9x/default.nix
@@ -0,0 +1,48 @@
+{ fasm, lib, stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation (finalAttr: {
+  pname = "patcher9x";
+  version = "0.8.50";
+
+  srcs = [
+    (fetchFromGitHub {
+      owner = "JHRobotics";
+      repo = "patcher9x";
+      rev = "v${finalAttr.version}";
+      hash = "sha256-TZw2+R7Dzojzxzal1Wp8jhe5gwU4CfZDROITi5Z+auo=";
+      name = "patcher9x";
+    })
+
+    (fetchFromGitHub {
+      owner = "JHRobotics";
+      repo = "nocrt";
+      rev = "f65cc7ef2a3cccd6264b2eb265d7fffbecb06ba4";
+      hash = "sha256-oeHcK9zYMDWk5sWfzYqLtC3MAJVtcaDJy4PvUGrxiPE=";
+      name = "nocrt";
+    })
+  ];
+
+  buildInputs = [ fasm ];
+  sourceRoot = finalAttr.pname;
+  hardeningDisable = [ "fortify" ];
+
+  preBuild = ''
+    rmdir nocrt
+    ln -s ../nocrt .
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp patcher9x $out/bin
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Patch for Windows 95/98/98 SE/Me to fix CPU issues";
+    homepage = "https://github.com/JHRobotics/patcher9x";
+    license = licenses.mit;
+    maintainers = with maintainers; [ hughobrien ];
+    platforms = platforms.linux;
+  };
+})
